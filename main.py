ff54f6e72f64b3ea4fa2deddbf582bcd3638c03f
Sandro Shavdatuashvili <sandro.shav@gmail.com>
2023-05-23 11:28:36 +0400

    finished project

--- main.py
+++ main.py
@@ -1,56 +1,55 @@
+from flask import Flask, render_template, request, json, redirect
 import redis
-from flask import Flask, render_template, request, redirect, json
 
 app = Flask(__name__)
 r = redis.Redis()
 
 
-@app.route('/')
+@app.route("/", methods=["GET", "POST"])
 def home():
     return render_template("map.html")
 
 
-@app.route('/add-marker', methods=['POST'])
-def add_marker():
-    req = request.form
-    location = req['lname']
-    latitude = req['latitude']
-    longitude = req['longitude']
-
-    print(location, latitude, longitude)
-
-    r.geoadd("points", [longitude, latitude, location])
-    r.set("last_loc_name", location)
-    return redirect('/')
-
-
-@app.route('/data')
+@app.route("/data")
 def data():
-    loc_name = r.get('last_loc_name')
-
+    loc_name = r.get("last_loc_name")
     if loc_name is None:
-        r.geoadd('points', ['-104.985784', '39.728206', 'parking'])
-        loc_name = 'parking'
+        r.geoadd("points", [-104.985784, 39.728206, "parking"])
+        loc_name = "parking"
 
-    all_location = r.georadiusbymember(name='points', member=loc_name, radius=4000, withcoord=True)
+    all_loc = r.georadiusbymember(name='points', member=loc_name, radius=40000, unit='mi', withcoord=True)
+    cord = dict()
 
-    coord = dict()
-
-    for loc in all_location:
+    for loc in all_loc:
         name = loc[0].decode("utf-8")
-        longitude = loc[1][0]
         latitude = loc[1][1]
+        longitude = loc[1][0]
 
-        coord[name] = {'lat': latitude, 'lng': longitude}
+        cord[name] = {"lat": latitude, "lng": longitude}
 
-    return json.dumps(coord)
+    return json.dumps(cord)
 
 
 @app.route("/last")
 def last_loc():
-    return r.get('last_loc_name').decode('utf-8')
+    return r.get("last_loc_name").decode("utf-8")
 
 
-if __name__ == '__main__':
+@app.route("/add-marker", methods=["POST"])
+def add_marker():
+    req = request.form
+    location = req["lname"]
+    latitude = req["latitude"]
+    longitude = req["longitude"]
+
+    print(location, latitude, location)
+
+    # r.geoadd("points", longitude, latitude, location)  # don't use this line in the code as shown in the video
+    r.geoadd("points", [longitude, latitude, location])  # redis-py api update so use this one
+    r.set("last_loc_name", location)
+
+    return redirect("/")
+
+
+if __name__ == "__main__":
     app.run()
-
